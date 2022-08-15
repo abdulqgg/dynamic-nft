1a936cd370e5a0d6320bad405d0d9ff83677f106
abdul gilani <abdul.gilani@gmail.com>
2022-08-15 16:30:06 +0100

    typo fix

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,13 +1,13 @@
 from scripts.helpful_scripts import get_account
-from brownie import DynamicNft
+from brownie import BullBear
 
 sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
 
 def deploy():
     account = get_account()
-    dynamic_nft = DynamicNft.deploy({"from": account})
-    tx = dynamic_nft.safeMint(account, sample_token_uri, {"from": account})
-    tx.wait(1)
+    dynamic_nft = BullBear.deploy({"from": account})
+    #tx = dynamic_nft.safeMint(account, sample_token_uri, {"from": account})
+    #tx.wait(1)
     print("success")
 
 
